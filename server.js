78d469e7cb4f08d243e0ec7056ad1a7770fdab6e
Osama Hamad <osamahama2005@gmail.com>
2021-10-21 16:10:51 -0400

    fix static file issue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 let express = require('express')
 let app = express()
 
-app.use(express.static('static'))
+app.use('/static', express.static('./static/'))
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/index.html')
