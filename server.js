5608cc0a36495e45bdf6628ff62d8d643a336a02
Osama Hamad <osamahama2005@gmail.com>
2021-10-21 15:27:08 -0400

    add listen at viewport 1010

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 let express = require('express')
 let app = express()
 
+app.use(express.static('static'))
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html')
+})
+
 app.listen(1010)
